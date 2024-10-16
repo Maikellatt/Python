723e10310cd47c58cf5fd4105e5f1b9f98a1b0e6
Maikel Lätt <maikellatt@Maikels-MacBook-Pro.local>
2024-10-16 17:26:22 +0300

    9 tehtud

--- yl9.py
+++ yl9.py
@@ -4,3 +4,16 @@
 # Lisaks tuleb kontrollida, kas etteantud küljepikkustega kolmnurk saab üldse eksisteerida. 
 # Külje pikkused ei pea olema täisarvud. (ujukomaarv - float)
 
+a = float(input('Sisesta kolmnurga esimese külg'))
+b = float(input('Sisesta kolmnurga teise külg'))
+c = float(input('Sisesta kolmnurga kolmas külg'))
+
+if not (a + b > c and a + c > b and b + c > a):
+    print('Kolmnurk ei saa eksisteerida')
+
+elif a == b == c:
+    print('Kolmnurk on võrdkülgne')
+elif a == b or a == c or b == c:
+    print('Kolmnurk on võrdhaarne')
+else:
+    print('Kolmnurk on erikülgne')
